c2df6e15889730090d3171347896e2318a11071b
Charansai Burra <charansaiburra5@gmail.com>
2026-02-23 22:18:58 +0530

    Removed basePath for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/portfolio-generator",
-  assetPrefix: "/portfolio-generator/",
+ //output: "export",
+  //basePath: "/portfolio-generator",
+  //assetPrefix: "/portfolio-generator/",
   images: {
     unoptimized: true,
   },
